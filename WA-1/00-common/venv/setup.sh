a21c90786e9805a66ec0249b62e29a484db285ff
dwl38 <dwl38@cam.ac.uk>
2025-03-07 17:35:21 +0000

    Updated venvs to use cuequivariance 0.2.0

--- WA-1/00-common/venv/setup.sh
+++ WA-1/00-common/venv/setup.sh
@@ -4,10 +4,10 @@ python -m venv ./
 source ./bin/activate
 sleep 5
 
-pip install torch
-pip install cuequivariance==0.1.0
-pip install cuequivariance-torch==0.1.0
-pip install cuequivariance-ops-torch-cu12==0.1.0
+pip install torch==2.6.0
+pip install cuequivariance==0.2.0
+pip install cuequivariance-torch==0.2.0
+pip install cuequivariance-ops-torch-cu12==0.2.0
 
 git clone https://github.com/ACEsuit/mace.git
 pip install ./mace
